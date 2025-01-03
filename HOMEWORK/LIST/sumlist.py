c2675dad397441723af94aea9ffe4e6a76a3cf53
krushi-koradiya <krushikoradiya@gmail.com>
2025-01-03 14:28:00 +0530

    initial commit

---
+++ HOMEWORK/LIST/sumlist.py
@@ -0,0 +1,18 @@
+# print sum of a list
+def sum_list(lst):
+    return sum(lst)
+l = [1, 2, 3, 4, 5]
+print("Sum is:",sum_list(l))
+
+# largest element in a list
+def large_list(lst1):
+    return max(lst1)
+l=[4,8,9,3]
+print("Largest element is:",large_list(l))
+
+
+# Remove duplicate in a list
+def duplicate_list(lst2):
+    return 
+
+
---
+++ List/list.py
@@ -0,0 +1,30 @@
+# 1.create a list
+l=[10,20,30]
+print(l)
+l1=["krushi","khushi","krisha"]
+print(l1)
+l2=["krushi",1,2]
+print(l2)
+
+
+# 2.print this through index
+marks=[30,54,89]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+
+
+# 3.use string and numerical value
+a=["asd",12,'c','d']
+print(a)
+print(type(a))
+
+# 4.using conditional statment
+b=[30,"pqr",'c']
+if b==30:
+    print(b)
+
+
+#using slicing 
+c=[] 
